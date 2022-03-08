f0c7fc98bb3c7b43322ff8e0a2c2c9abc75df392
Daniel Pascual <daniel.s.pascual@gmail.com>
2022-03-08 13:59:23 +0100

    WIP

--- misp_modules/modules/expansion/virustotal.py
+++ misp_modules/modules/expansion/virustotal.py
@@ -80,16 +80,6 @@ class VirusTotalParser(object):
                                     'sha256': self.parse_hash, 'url': self.parse_url}
         self.proxies = None
 
-    def query_api(self, attribute):
-        self.attribute.from_dict(**attribute)
-        misp_object = self.input_types_mapping[self.attribute.type](self.attribute.value)
-        self.misp_event.add_object(**misp_object)
-
-    def get_result(self):
-        event = json.loads(self.misp_event.to_json())
-        results = {key: event[key] for key in ('Attribute', 'Object') if (key in event and event[key])}
-        return {'results': results}
-
     @staticmethod
     def get_total_analysis(_, analysis, verdict):
         if not analysis:
@@ -107,11 +97,33 @@ class VirusTotalParser(object):
         attribute.from_dict(**dict(type=attr_type, value=value))
         return attribute
 
+    def query_api(self, attribute):
+        self.attribute.from_dict(**attribute)
+        misp_object = self.input_types_mapping[self.attribute.type](self.attribute.value)
+        self.misp_event.add_object(**misp_object)
+
+    def get_result(self):
+        event = json.loads(self.misp_event.to_json())
+        results = {key: event[key] for key in ('Attribute', 'Object') if (key in event and event[key])}
+        return {'results': results}
+
+    def parse_vt_object(self, query_result):
+        data = query_result['data']['attributes']
+        analysis = data['last_analysis_results']
+
+        vt_object = MISPObject('virustotal-report')
+        vt_object.add_attribute('permalink', type='link', value=query_result['data']['links']['self'])
+        detection_ratio = '{}/{}'.format(analysis['malicious'],
+                                         self.get_total_analysis(analysis, data.get('trusted_verdict')))
+        vt_object.add_attribute('detection-ratio', type='text', value=detection_ratio, disable_correlation=True)
+        self.misp_event.add_object(**vt_object)
+        return vt_object.uuid
+
     ################################################################################
     ####                         Main parsing functions                         #### # noqa
     ################################################################################
 
-    def parse_domain_new(self, domain):
+    def parse_domain(self, domain):
         response = self.client.get_domain_report(domain)
         data = response['data']['attributes']
 
@@ -169,9 +181,9 @@ class VirusTotalParser(object):
             url_object.add_reference(domain_ip_object.uuid, 'hosted-in')
             self.misp_event.add_object(**url_object)
 
-        self.misp_event.add_object(**domain_ip_object)
+        return domain_ip_object
 
-    def parse_hash_new(self, file_hash):
+    def parse_hash(self, file_hash):
         response = self.client.get_file_report(file_hash)
         data = response['data']['attributes']
         vt_uuid = self.parse_vt_object(response)
@@ -183,148 +195,38 @@ class VirusTotalParser(object):
 
         return file_object
 
-    def parse_domain(self, domain, recurse=False):
-        req = requests.get(self.base_url.format('domain'), params={'apikey': self.apikey, 'domain': domain}, proxies=self.proxies)
-        if req.status_code != 200:
-            return req.status_code
-        req = req.json()
-        hash_type = 'sha256'
-        whois = 'whois'
-        feature_types = {'communicating': 'communicates-with',
-                         'downloaded': 'downloaded-from',
-                         'referrer': 'referring'}
-        siblings = (self.parse_siblings(domain) for domain in req['domain_siblings'])
-        uuid = self.parse_resolutions(req['resolutions'], req['subdomains'] if 'subdomains' in req else None, siblings)
-        for feature_type, relationship in feature_types.items():
-            for feature in ('undetected_{}_samples', 'detected_{}_samples'):
-                for sample in req.get(feature.format(feature_type), [])[:self.limit]:
-                    status_code = self.parse_hash(sample[hash_type], False, uuid, relationship)
-                    if status_code != 200:
-                        return status_code
-        if req.get(whois):
-            whois_object = MISPObject(whois)
-            whois_object.add_attribute('text', type='text', value=req[whois])
-            self.misp_event.add_object(**whois_object)
-        return self.parse_related_urls(req, recurse, uuid)
+    def parse_ip(self, ip):
+        response = self.client.get_ip_report(ip)
+        data = response['data']['attributes']
 
-    def parse_hash(self, sample, recurse=False, uuid=None, relationship=None):
-        req = requests.get(self.base_url.format('file'), params={'apikey': self.apikey, 'resource': sample}, proxies=self.proxies)
-        status_code = req.status_code
-        if req.status_code == 200:
-            req = req.json()
-            vt_uuid = self.parse_vt_object(req)
-            file_attributes = []
-            for hash_type in ('md5', 'sha1', 'sha256'):
-                if req.get(hash_type):
-                    file_attributes.append({'type': hash_type, 'object_relation': hash_type,
-                                            'value': req[hash_type]})
-            if file_attributes:
-                file_object = MISPObject('file')
-                for attribute in file_attributes:
-                    file_object.add_attribute(**attribute)
-                file_object.add_reference(vt_uuid, 'analyzed-with')
-                if uuid and relationship:
-                    file_object.add_reference(uuid, relationship)
-                self.misp_event.add_object(**file_object)
-        return status_code
+        # DOMAIN
+        ip_object = MISPObject('domain-ip')
+        ip_object.add_attribute('ip', type='ip-dst', value=self.attribute.value)
 
-    def parse_ip(self, ip, recurse=False):
-        req = requests.get(self.base_url.format('ip-address'), params={'apikey': self.apikey, 'ip': ip}, proxies=self.proxies)
-        if req.status_code != 200:
-            return req.status_code
-        req = req.json()
-        if req.get('asn'):
-            asn_mapping = {'network': ('ip-src', 'subnet-announced'),
-                           'country': ('text', 'country')}
-            asn_object = MISPObject('asn')
-            asn_object.add_attribute('asn', type='AS', value=req['asn'])
-            for key, value in asn_mapping.items():
-                if req.get(key):
-                    attribute_type, relation = value
-                    asn_object.add_attribute(relation, type=attribute_type, value=req[key])
-            self.misp_event.add_object(**asn_object)
-        uuid = self.parse_resolutions(req['resolutions']) if req.get('resolutions') else None
-        return self.parse_related_urls(req, recurse, uuid)
+        # ASN
+        asn_object = MISPObject('asn')
+        asn_object.add_attribute('asn', type='AS', value=data.get('asn'))
+        asn_object.add_attribute('subnet-announced', type='ip-src', value=data.get('network'))
+        asn_object.add_attribute('country', type='text', value=data.get('country'))
+        self.misp_event.add_object(**asn_object)
 
-    def parse_url(self, url, recurse=False, uuid=None):
-        req = requests.get(self.base_url.format('url'), params={'apikey': self.apikey, 'resource': url}, proxies=self.proxies)
-        status_code = req.status_code
-        if req.status_code == 200:
-            req = req.json()
-            vt_uuid = self.parse_vt_object(req)
-            if not recurse:
-                feature = 'url'
-                url_object = MISPObject(feature)
-                url_object.add_attribute(feature, type=feature, value=url)
-                url_object.add_reference(vt_uuid, 'analyzed-with')
-                if uuid:
-                    url_object.add_reference(uuid, 'hosted-in')
-                self.misp_event.add_object(**url_object)
-        return status_code
+        # RESOLUTIONS
+        resolutions = self.client.get_ip_relatioship(ip, 'resolutions')
+        for resolution in resolutions['data']:
+            ip_object.add_attribute('domain', type='domain', value=resolution['attributes']['host_name'])
 
-    ################################################################################
-    ####                      Additional parsing functions                      #### # noqa
-    ################################################################################
-
-    def parse_related_urls(self, query_result, recurse, uuid=None):
-        if recurse:
-            for feature in ('detected_urls', 'undetected_urls'):
-                if feature in query_result:
-                    for url in query_result[feature]:
-                        value = url['url'] if isinstance(url, dict) else url[0]
-                        status_code = self.parse_url(value, False, uuid)
-                        if status_code != 200:
-                            return status_code
-        else:
-            for feature in ('detected_urls', 'undetected_urls'):
-                if feature in query_result:
-                    for url in query_result[feature]:
-                        value = url['url'] if isinstance(url, dict) else url[0]
-                        self.misp_event.add_attribute('url', value)
-        return 200
-
-    def parse_resolutions(self, resolutions, subdomains=None, uuids=None):
-        domain_ip_object = MISPObject('domain-ip')
-        if self.attribute.type in ('domain', 'hostname'):
-            domain_ip_object.add_attribute('domain', type='domain', value=self.attribute.value)
-            attribute_type, relation, key = ('ip-dst', 'ip', 'ip_address')
-        else:
-            domain_ip_object.add_attribute('ip', type='ip-dst', value=self.attribute.value)
-            attribute_type, relation, key = ('domain', 'domain', 'hostname')
-        for resolution in resolutions:
-            domain_ip_object.add_attribute(relation, type=attribute_type, value=resolution[key])
-        if subdomains:
-            for subdomain in subdomains:
-                attribute = MISPAttribute()
-                attribute.from_dict(**dict(type='domain', value=subdomain))
-                self.misp_event.add_attribute(**attribute)
-                domain_ip_object.add_reference(attribute.uuid, 'subdomain')
-        if uuids:
-            for uuid in uuids:
-                domain_ip_object.add_reference(uuid, 'sibling-of')
-        self.misp_event.add_object(**domain_ip_object)
-        return domain_ip_object.uuid
-
-    def parse_siblings(self, domain):
-        attribute = MISPAttribute()
-        attribute.from_dict(**dict(type='domain', value=domain))
-        self.misp_event.add_attribute(**attribute)
-        return attribute.uuid
-
-    def parse_vt_object(self, query_result):
-        data = query_result['data']['attributes']
-        analysis = data['last_analysis_results']
-
-        vt_object = MISPObject('virustotal-report')
-        vt_object.add_attribute('permalink', type='link', value=query_result['data']['links']['self'])
-        detection_ratio = '{}/{}'.format(analysis['malicious'],
-                                         self.get_total_analysis(analysis, data.get('trusted_verdict')))
-        vt_object.add_attribute('detection-ratio', type='text', value=detection_ratio, disable_correlation=True)
-        self.misp_event.add_object(**vt_object)
-        return vt_object.uuid
+        # URLS
+        urls = self.client.get_domain_relatioship(ip, 'urls')
+        for url in urls['data']:
+            vt_uuid = self.parse_vt_object(url['attributes'])
+            url_object = MISPObject('url')
+            url_object.add_attribute('url', type='url', value=url['attributes']['url'])
+            url_object.add_reference(vt_uuid, 'analyzed-with')
+            url_object.add_reference(ip_object.uuid, 'hosted-in')
+            self.misp_event.add_object(**url_object)
 
 
-def get_proxy_settings(self, config: dict) -> dict:
+def get_proxy_settings(config: dict) -> dict:
     """Returns proxy settings in the requests format.
     If no proxy settings are set, return None."""
     proxies = None
